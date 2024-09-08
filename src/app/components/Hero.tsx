2400c8b088166f03b4d3fe2d00b6c32dd2dd42fa
Nadun Sumedha <90174385+NadunKMS@users.noreply.github.com>
2024-09-08 15:21:00 +0530

    2024 mid year update

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,32 +1,36 @@
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 function Hero() {
   return (
-    <div className='flex flex-col items-center py-40'>
-        <Image 
+    <div className="flex flex-col items-center py-40">
+      <Image
         src="/Name-card.webp"
         width={300}
         height={200}
         alt="Picture of the author"
-        className='py-4'
-        />
-        <h2 className='md:text-4xl text-3xl font-bold text-center'>
-            Let’s Transform Your Online <br/> Presence with Endless <br/> Creativity
-        </h2>
+        className="py-4"
+      />
+      <h2 className="md:text-4xl text-3xl font-bold text-center">
+        Let’s Transform Your Online <br /> Presence with Endless <br />{" "}
+        Creativity
+      </h2>
 
-        <div className='space-y-6 my-6 text-center'>
-          <p className="text-gray-500">
-           I focus on create high-converting websites that drive <br/> business growth and elevate your brand's  <br/> online presence.
-          </p>
-        </div>
-        {/* <div className="absolute inset-0 z-10"></div> */}
+      <div className="space-y-6 my-6 text-center">
+        <p className="text-gray-500">
+          I focus on create high-converting websites that drive <br /> business
+          growth and elevate your brand's <br /> online presence.
+        </p>
+      </div>
+      {/* <div className="absolute inset-0 z-10"></div> */}
 
-        <button className='cta hover-animate'>
-          <Link href='https://cal.com/nadun'>Schedule a Free Strategy Call</Link>
+      <Link href="https://cal.com/nadun">
+        <button className="cta hover-animate">
+          Schedule a Free Strategy Call
         </button>
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
