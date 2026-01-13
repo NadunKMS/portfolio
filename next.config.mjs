b1cbb15baa8becce2bf784f7390e84a4f6ee22de
itzmenadun <kmnadun2003@gmail.com>
2026-01-13 17:24:43 +0530

    Major Update

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Ignore ESLint errors during builds
-    ignoreDuringBuilds: true,
-  },
-};
-
-export default nextConfig;
